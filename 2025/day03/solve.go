8ebe639acbefe5de1d2c5f979bb55f88c1e22f12
Emerson Matthews <emerson@recurse.me>
2025-12-04 18:34:14 -0500

    solve 2025-03

--- 2025/day03/solve.go
+++ 2025/day03/solve.go
@@ -1,11 +1,57 @@
 package day03
 
+import (
+	"slices"
+	"strconv"
+	"strings"
+)
+
 func SolvePuzzle1(input string) int {
-	// TODO: solve puzzle 1
-	return 0
+	banks := parseInput(input)
+	totalJoltage := 0
+
+	for _, bank := range banks {
+		firstMax := slices.Max(bank[:len(bank)-1])
+		i := slices.Index(bank, firstMax)
+		secondMax := slices.Max(bank[i+1:])
+		joltage, _ := strconv.Atoi(string(rune(firstMax+'0')) + string(rune(secondMax+'0')))
+		println("joltage", firstMax, secondMax, joltage)
+		totalJoltage += joltage
+	}
+
+	return totalJoltage
 }
 
 func SolvePuzzle2(input string) int {
-	// TODO: solve puzzle 2
-	return 0
+	banks := parseInput(input)
+	totalJoltage := 0
+
+	for _, bank := range banks {
+		start := 0
+		joltageStr := ""
+		for i := 0; i < 12; i++ {
+			max := slices.Max(bank[start : len(bank)-(11-i)])
+			joltageStr += strconv.Itoa(max)
+			start += slices.Index(bank[start:], max) + 1
+		}
+		joltage, _ := strconv.Atoi(joltageStr)
+		println("joltage", joltage)
+		totalJoltage += joltage
+	}
+
+	return totalJoltage
+}
+
+func parseInput(input string) [][]int {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	banks := make([][]int, len(lines))
+
+	for i, line := range lines {
+		line = strings.TrimSpace(line)
+		banks[i] = make([]int, len(line))
+		for j, char := range line {
+			banks[i][j] = int(char - '0')
+		}
+	}
+	return banks
 }

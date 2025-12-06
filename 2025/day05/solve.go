c114f451af6327dc14d61b00e306ef700a32d677
Emerson Matthews <emerson@recurse.me>
2025-12-05 20:07:04 -0500

    solve 2025-05

--- 2025/day05/solve.go
+++ 2025/day05/solve.go
@@ -1,23 +1,82 @@
 package day05
 
-import "strings"
+import (
+	"slices"
+	"spissable/advent-of-go-template/utils"
+	"strconv"
+	"strings"
+)
 
 func SolvePuzzle1(input string) int {
-	board := parseInput(input)
-	return len(board)
+	ranges, ids := parseInput(input)
+	numFresh := 0
+	for _, id := range ids {
+		for _, r := range ranges {
+			if id >= r[0] && id <= r[1] {
+				numFresh++
+				break
+			}
+		}
+	}
+	return numFresh
 }
 
 func SolvePuzzle2(input string) int {
-	board := parseInput(input)
-	return len(board)
+	ranges, _ := parseInput(input)
+	numFresh := 0
+	for _, r := range ranges {
+		numFresh += r[1] - r[0] + 1
+	}
+	return numFresh
 }
 
-func parseInput(input string) [][]string {
+func parseInput(input string) ([][2]int, []int) {
 	lines := strings.Split(strings.TrimSpace(input), "\n")
-	board := make([][]string, len(lines))
+	ranges := make([][2]int, 0)
+	ids := make([]int, 0)
 
-	for i, line := range lines {
-		board[i] = strings.Split(strings.TrimSpace(line), "")
+	for _, line := range lines {
+		if strings.Contains(line, "-") {
+			vals := strings.Split(line, "-")
+			ranges = append(ranges, [2]int{
+				utils.Must(strconv.Atoi(strings.TrimSpace(vals[0]))),
+				utils.Must(strconv.Atoi(strings.TrimSpace(vals[1]))),
+			})
+		} else if len(strings.TrimSpace(line)) > 0 {
+			ids = append(ids, utils.Must(strconv.Atoi(strings.TrimSpace(line))))
+		}
 	}
-	return board
+	return combineRanges(ranges), ids
+}
+
+func combineRanges(ranges [][2]int) [][2]int {
+	combined := make([][2]int, 0)
+	for _, r := range ranges {
+		segmented := true
+		for j, c := range combined {
+			lhs := inRange(c[0], r[0], r[1])
+			rhs := inRange(c[1], r[0], r[1])
+			if lhs || rhs {
+				segmented = false
+				c[0] = min(c[0], r[0])
+				c[1] = max(c[1], r[1])
+				combined[j] = c
+				break
+			}
+		}
+		if segmented {
+			combined = append(combined, r)
+		}
+	}
+	if len(ranges) > len(combined) {
+		println("turned", len(ranges), "ranges into", len(combined), "ranges")
+		// bit hacky but this works...
+		slices.Reverse(combined)
+		return combineRanges(combined)
+	}
+	return combined
+}
+
+func inRange(x, r1, r2 int) bool {
+	return x >= r1 && x <= r2
 }
